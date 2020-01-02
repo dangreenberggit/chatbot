589b09333ae5fd5656724c2ee34776d758eba85a
dangreenberggit <dgreenberg1987@gmail.com>
2020-01-02 15:20:48 +0200

    removed cornell movie dialogue corpus, added google crawl for queries

--- boto.py
+++ boto.py
@@ -5,6 +5,7 @@ from bottle import route, run, template, static_file, request
 import json
 import random
 import chats
+import google_search
 
 
 @route('/', method='GET')
@@ -20,17 +21,49 @@ def chat():
 
 def boto_response(user_message):
 
-    user_message.split()
-    if any(user_message in chats.bot_hearing["swearing"]):
-        animation: "no"
-        boto_message = random.choice(chats.bot_saying["swearing"])
+    user_message.lower()
+    boto_text = "no response assigned"
+    swear_counter = 0
 
-    for string in chats.bot_hearing["afraid"]:
-        if string in user_message:
-            animation = "afraid"
-            boto_message = random.choice(chats.bot_saying["afraid"])
+    split_user_message = user_message.split(" ")
 
-    return {"animation": animation, "msg": boto_message}
+    if swear_counter == 3:
+        animation = "takeoff"
+        boto_text = "That's it. Screw you guys, I'm going home."
+
+    if any(swear in split_user_message for swear in chats.bot_hearing["swearing"]):
+        animation = "no"
+        print("curse registered")
+        boto_text = random.choice(chats.bot_saying["swearing"])
+        swear_counter += 1
+
+    if check_search(user_message):
+        animation = "excited"
+        boto_text = google_search.chatbot_query(user_message)
+
+    if check_keywords(user_message, chats.bot_hearing["afraid"]):
+        animation = "afraid"
+        boto_text = random.choice(chats.bot_saying["afraid"])
+
+    return {"animation": animation, "msg":boto_text}
+
+def check_search(message):
+    search_triggers = ["who", "what", "when", "why"]
+    if ("you know" in message) or (not "you" in message):
+        for word in search_triggers:
+            if word in message:
+                return True
+    elif "where" in message:
+        return False
+    elif "?" in message and not ("you" or "i") in message:
+        return True
+    else:
+        return False
+
+def check_keywords(message, hearing):
+    for word in hearing:
+        if word in message:
+            return True
 
 @route("/test", method='POST')
 def chat():
--- chats.py
+++ chats.py
@@ -1,9 +1,12 @@
 bot_hearing = {
-    "swearing": "fuck",
-    "afraid": "snakes",
+    "swearing": ["fuck", "fucking", "shit", "ass", "shit"],
+    "afraid": ["snakes", "ghosts", "die", "destroy", "meteor", "global warming", "trump", "disease"],
+    "bored": ["boring", "CSS", "philosophy"]
+
 }
 
 bot_saying = {
-    "swearing": "no, fuck you, sir",
-    "afraid": ["oh em gee!", "literally shaking rn"],
+    "swearing": ["fuck you, sir", "you're quite mean!", "how rude!"],
+    "afraid": ["oh em gee!", "literally shaking rn", "that sounds so scary, please hold me"],
+    "bored": ["I'm getting bored", "zzzz"]
 }
